f3fef4c2f2a8f1258561dae675987ac290caf029
bingtian.yu <bingtian.yu@vhall.com>
2020-10-30 11:34:49 +0800

    增加公共方法

---
+++ routes/common.js
@@ -0,0 +1,12 @@
+const baseUrl = 'http://localhost:8989/'
+
+module.exports.WorkType = {
+    1 : '前端',
+    2 : '后端'
+}
+module.exports.GoUrl = {
+    baseUrl: baseUrl,
+    workUrl : baseUrl + 'work',
+    projectUrl : baseUrl + 'project',
+    userUrl : baseUrl + 'user',
+}
