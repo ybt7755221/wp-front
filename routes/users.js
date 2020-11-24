69d15a7fe0a3e4993779987c4a70a19dbf738c57
bingtian.yu <bingtian.yu@vhall.com>
2020-11-24 18:26:52 +0800

    修改user部分

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 const session = require('express-session');
 var request = require('request');
 const { runInNewContext } = require('vm');
+const etools = require('../library/etools');
+const {http_request} = require('../library/etools');
 var router = express.Router();
 var tools = require('./common')
 
@@ -13,25 +15,20 @@ router.get('/logout', function(req, res, next) {
     req.session.destroy();
     res.redirect('/');
 });
-router.post('/login', function(req, res, next) {
-  request.post(
-    tools.GoUrl.baseUrl+'check',
-    { form: req.body },
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var bodyJson = JSON.parse(body);
-        if (bodyJson.code == 1000) {
-          delete bodyJson.data.password
-          req.session.userInfo = bodyJson.data
-          res.redirect('/pages/')
-        }else{
-          res.send(bodyJson)
-        }
-      }else{
-        res.send(response)
-      }
+router.post('/login', async function(req, res, next) {
+  let resp = await http_request(tools.GoUrl.baseUrl+'check', req.body, "POST");
+  if (resp['error'] == null && resp['data'].statusCode == 200) {
+    var bodyJson = etools.s2j(resp['data'].body);
+    if (bodyJson.code == 1000) {
+      delete bodyJson.data.password
+      req.session.userInfo = bodyJson.data
+      res.redirect('/pages/')
+    }else{
+      res.send(bodyJson)
     }
-  );
+  }else{
+    res.send(resp['error'])
+  }
 })
 
 module.exports = router;
